63a30f210d0a2e55643e21162bcf80eafa35e4f1
Ovi sanjaya <ovi.sanjaya11@gmail.com>
2022-04-13 20:27:22 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigFile("app/config.json")
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func main() {
+
+}
