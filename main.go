e93e073322581f8776c973d73af9300f912211aa
Ovi sanjaya <ovi.sanjaya11@gmail.com>
2022-04-15 13:02:25 +0700

    up

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo/v4"
+
+	// "github.com/labstack/echo/v4/middleware"
 	"github.com/spf13/viper"
 )
 
@@ -15,5 +19,12 @@ func init() {
 }
 
 func main() {
-
+	// config := got
+	// db connection
+	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
+	e.Pre(middleware.RemoveTrailingSlash())
 }
---
+++ model/database/database.go
@@ -0,0 +1,9 @@
+package database
+
+type LeaderBoard struct {
+	IdUser   int
+	UserName string
+	FishName string
+	Score    int
+	Feedback string
+}
